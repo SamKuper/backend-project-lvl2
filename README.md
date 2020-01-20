3818da80427ab63e230a5b6504ec86620d4e71f0
Kuper <13lars.cooper@gmail.com>
2020-01-20 22:53:58 +0300

    step3

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/7b53eb21719a8a72afcd/maintainability)](https://codeclimate.com/github/SamKuper/backend-project-lvl2/maintainability)
 [![Build Status](https://travis-ci.org/SamKuper/backend-project-lvl2.svg?branch=master)](https://travis-ci.org/SamKuper/backend-project-lvl2)
+[![asciicast](https://asciinema.org/a/LQRFeVXqAWRkrEWXmW6dcoNUo.svg)](https://asciinema.org/a/LQRFeVXqAWRkrEWXmW6dcoNUo)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-module.exports = (file1, file2) => `${file1}, ${file2}`;
+const fs = require('fs');
+
+module.exports = (file1, file2) => {
+  const data1 = JSON.parse(fs.readFileSync(file1));
+  const data2 = JSON.parse(fs.readFileSync(file2));
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const oldFields = keys1.reduce((acc, key) => {
+    if (keys2.includes(key)) {
+      return data1[key] === data2[key]
+        ? [...acc, `${key}: ${data1[key]}`]
+        : [...acc, `+${key}: ${data2[key]}`, `-${key}: ${data1[key]}`];
+    }
+    return [...acc, `-${key}: ${data1[key]}`];
+  }, []);
+  const newFields = keys2.reduce((acc, key) => (keys1.includes(key)
+    ? [...acc] : [...acc, `+${key}: ${data2[key]}`]),
+  oldFields);
+  return ['{', ...newFields, '}'].join('\n');
+};
