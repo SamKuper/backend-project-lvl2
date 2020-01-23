7bd11993885c25cf851c053d00b00d2373c08469
Kuper <13lars.cooper@gmail.com>
2020-01-23 18:42:38 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,23 +2,27 @@ const fs = require('fs');
 
 const getObject = (file) => JSON.parse(fs.readFileSync(file));
 
+const getDiff = (firstConfig, secondConfig, acc) => {
+  const diff = Object.keys(firstConfig).reverse()
+    .reduce((iacc, key) => {
+      if (secondConfig[key]) {
+        return secondConfig[key] === firstConfig[key]
+          ? [`    ${key}: ${firstConfig[key]}`, ...iacc]
+          : [`  + ${key}: ${secondConfig[key]}`, `  - ${key}: ${firstConfig[key]}`, ...iacc];
+      }
+      return [`  - ${key}: ${firstConfig[key]}`, ...iacc];
+    }, acc);
+  return diff;
+};
+
 const compare = (file1, file2) => {
   const firstConfig = getObject(file1);
   const secondConfig = getObject(file2);
   const newFields = Object.keys(secondConfig)
     .reduce((acc, key) => (firstConfig[key]
       ? [...acc] : [...acc, `  + ${key}: ${secondConfig[key]}`]), []);
-  const differences = Object.keys(firstConfig)
-    .reverse()
-    .reduce((acc, key) => {
-      if (secondConfig[key]) {
-        return secondConfig[key] === firstConfig[key]
-          ? [`    ${key}: ${firstConfig[key]}`, ...acc]
-          : [`  + ${key}: ${secondConfig[key]}`, `  - ${key}: ${firstConfig[key]}`, ...acc];
-      }
-      return [`  - ${key}: ${firstConfig[key]}`, ...acc];
-    }, newFields);
-  return ['{', ...differences, '}'].join('\n');
+  const diff = getDiff(firstConfig, secondConfig, newFields);
+  return ['{', ...diff, '}'].join('\n');
 };
 
 module.exports = compare;
