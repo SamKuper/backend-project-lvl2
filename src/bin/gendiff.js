d553cbf9983c6c9981db363bb79058b0881655c5
Kuper <13lars.cooper@gmail.com>
2020-02-05 20:17:42 +0300

    fix bug

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,8 +6,8 @@ program.version('1.0.2')
   .description('Compares two configuration files and shows a difference')
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
-  .action((firstConfig, secondConfig, format) => {
-    console.log(compare(firstConfig, secondConfig, format));
+  .action((firstConfig, secondConfig) => {
+    console.log(compare(firstConfig, secondConfig, program.format));
   });
 
 program.parse(process.argv);
