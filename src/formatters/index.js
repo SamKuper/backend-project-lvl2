0e87bfe891050a2977b945b0b2a3bea83026b4c4
Kuper <13lars.cooper@gmail.com>
2020-02-11 20:30:54 +0300

    naming

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,10 @@
 import getPlain from './toPlain';
 import getObject from './toObject';
 
-const render = {
+const renders = {
   object: (ast) => getObject(ast),
   plain: (ast) => getPlain(ast),
   json: (ast) => JSON.stringify(ast),
 };
 
-export default (ast, format) => render[format](ast);
+export default (ast, format) => renders[format](ast);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import fs from 'fs';
 import path from 'path';
-import parser from './parsers';
+import parse from './parsers';
 import getAst from './buildAst';
 import render from './formatters';
 
-const getObject = (file, format) => parser(file, format);
+const getObject = (file, format) => parse(file, format);
 const getFormat = (str) => path.extname(str).split('').slice(1).join('');
 
 export default (filePath1, filePath2, outputFormat = 'object') => {
   const firstFile = fs.readFileSync(filePath1, 'utf-8');
   const secondFile = fs.readFileSync(filePath2, 'utf-8');
-  const format1 = getFormat(filePath1);
-  const format2 = getFormat(filePath2);
-  const firstConfig = getObject(firstFile, format1);
-  const secondConfig = getObject(secondFile, format2);
-  const diff = getAst(firstConfig, secondConfig);
-  return render(diff, outputFormat);
+  const firstFileFormat = getFormat(filePath1);
+  const secondFileFormat = getFormat(filePath2);
+  const firstObject = getObject(firstFile, firstFileFormat);
+  const secondObject = getObject(secondFile, secondFileFormat);
+  const diffTree = getAst(firstObject, secondObject);
+  return render(diffTree, outputFormat);
 };
