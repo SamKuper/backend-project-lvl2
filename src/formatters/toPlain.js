02a7ce8c3e1515888fd921a981e25374621e806c
Kuper <13lars.cooper@gmail.com>
2020-02-18 04:02:21 +0300

    refactor

--- src/formatters/toPlain.js
+++ src/formatters/toPlain.js
@@ -6,8 +6,8 @@ const types = {
   added: (node, path) => `Property '${path}' was added with value: '${getValue(node.newValue)}'`,
   removed: (node, path) => `Property '${path}' was deleted`,
   changed: (node, path) => `Property '${path}' was changed from '${getValue(node.oldValue)}' to '${getValue(node.newValue)}'`,
-  nested: (node, path, fn) => fn(node.children, path).filter((n) => n !== '').join('\n'),
-  stable: () => '',
+  nested: (node, path, fn) => fn(node.children, path).filter((n) => n !== null).join('\n'),
+  stable: () => null,
 };
 
 const getPlain = (ast) => {
